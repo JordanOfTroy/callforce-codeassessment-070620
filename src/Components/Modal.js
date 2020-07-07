cbb8d7cc6be7e64d2ae488179526c6fdb69244fe
Jordan Smithson <jordantroysmithson@gmail.com>
2020-07-07 16:52:02 -0600

    Modal ahs been built out. aStill needs logic and functionality.

--- src/Components/Modal.js
+++ src/Components/Modal.js
@@ -2,9 +2,68 @@ import React from 'react'
 import '../styles/modal.css'
 
 const Modal = () => {
+
+    const handleChange = () => {
+        console.log('you are handling the change well')
+    }
+
+    const handleClick = () => {
+        console.log('you are handling the click well')
+    }
+
+    const closeModal = () => {
+        console.log('you are closing the modal')
+    }
+
     return (
-        <div className='modal'> 
-            <h1>Modal</h1>
+        <div className='modalOverlay centerMyStuff'> 
+            <div className='modalBody'>
+               <div className='row p-2'>
+                    <div className='col-12'>
+                        <i class="fal fa-times-hexagon"
+                            onClick={closeModal}
+                        ></i>
+                    </div>
+               </div>
+               <div className='row'>
+                    <div className='col-12 d-flex flex-column align-items-center justify-content-center'>
+                        <div className='modalInputGroup p-2 mb-2'>
+                            <p className='pb-1'>Please select Title</p>
+                            <select className='modalInput'>
+                                <option value='Doctor'>Doctor</option>
+                                <option value='Assistant'>Assistant</option>
+                                <option value='Hygienist'>Hygienist</option>
+                            </select>
+                        </div>
+                        <div className='modalInputGroup p-2 mb-2 startTimeGroup'>
+                            <p className='pb-1'>Please select Start Time</p>
+                            <input className='modalInput'
+                                type='datetime-local'
+                                id='startInput'
+                                name='startInput'
+                                onChange={handleChange}
+                                />
+                        </div>
+                        <div className='modalInputGroup p-2 mb-2 endTimeGroup'>
+                            <p className='pb-1'>Please select End Time</p>
+                            <input className='modalInput'
+                                type='datetime-local'
+                                id='endInput'
+                                name='endInput'
+                                onChange={handleChange}
+                                />
+                        </div>
+                    </div>
+               </div>
+               <div className='row'>
+                    <div className='col-12 d-flex justify-content-end'>
+                        <button className='btn btn-info mr-3 mt-2'
+                            type='submit'
+                            onClick={handleClick}
+                        >Submit</button>
+                    </div>
+               </div>
+            </div>
         </div>
     )
 }
