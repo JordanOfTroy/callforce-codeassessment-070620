38f289ca974d22c7e74994e34014a3ebe4ac1483
Jordan Smithson <jordantroysmithson@gmail.com>
2020-07-07 12:48:58 -0600

    FontAweseom kit added, company logo added and displaying. Working on connecting add button to modal.

--- src/Components/CalendarBody.js
+++ src/Components/CalendarBody.js
@@ -3,11 +3,14 @@ import '../styles/calendar.css'
 import TitleTable from './Title/TitleTable'
 import DateTable from './Date/DateTable'
 import CalendarTable from './Calendar/CalendarTable'
+import logo from '../media/logo-large.png'
 
 const CalendarBody = () => {
     return (
         <div className='calendarBody'>
             <div className='titleGroup'>
+                <img src={logo} alt='CallForce Logo' width='150'/>
+                <br/>
                 <button><i class="fas fa-plus"></i> Add Availability</button>
                 <TitleTable/>
             </div>
